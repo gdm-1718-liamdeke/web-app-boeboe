d08dd3d617c4679adec5174b507d70d5202ceb96
gdm-1718-liamdeke <liamdeke@student.arteveldehs.be>
2018-06-18 00:20:10 +0200

    transfer data with sessionstorage subpages fix

--- js/sessionstorageSet.js
+++ js/sessionstorageSet.js
@@ -1,7 +1,11 @@
-function namePatient() {
+function nameSet(){
+    
+    /** set patient name **/
     $naam = sessionStorage.getItem("patientNaam");
-    if($naam != null) {
-        document.getElementById("naamPatient").innerHTML = $naam;
+    if($naam !== null || $naam !== "") {
+        document.getElementById("ochtendzorgNaam").innerHTML = $naam ;
     }
     console.log($naam);
-}
+} 
+
+setTimeout(nameSet, 0);
